1990c4e2fd1c0a512658901fcd01eb6c3b7e7e67
Mirosław Malik <mmiro390@gmail.com>
2019-10-02 13:51:01 +0200

    add ngOnDestroy method to income-tax.component

--- src/app/cars/total-cost/income-tax/income-tax.component.ts
+++ src/app/cars/total-cost/income-tax/income-tax.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CostSharedService } from '../../cost-shared.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-income-tax',
   templateUrl: './income-tax.component.html',
   styleUrls: ['./income-tax.component.less']
 })
-export class IncomeTaxComponent implements OnInit {
+export class IncomeTaxComponent implements OnInit, OnDestroy {
 
 private incomeTax = 18;
 income: number;
+
+costSubscription: Subscription;
 
   constructor(
     private costSharedService: CostSharedService
@@ -17,9 +20,15 @@ income: number;
 
   ngOnInit() {
 
-    this.costSharedService.totalCostSource$.subscribe(cost => {
+    this.costSubscription = this.costSharedService.totalCostSource$.subscribe(cost => {
       this.income = cost * this.incomeTax / 100;
     });
+  }
+
+  ngOnDestroy() {
+    if (this.costSubscription) {
+      this.costSubscription.unsubscribe();
+    }
   }
 
 }
